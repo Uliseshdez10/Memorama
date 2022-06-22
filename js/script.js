29c4268751b1c948ce060b4878d05ee53b8369dc
Uliseshdez10 <83787400+Uliseshdez10@users.noreply.github.com>
2022-06-22 12:46:40 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ const MAXIMOS_INTENTOS = 8, // Intentos máximos que tiene el jugador
     COLUMNAS = 4, // Columnas del memorama
     SEGUNDOS_ESPERA_VOLTEAR_IMAGEN = 1, // Por cuántos segundos mostrar ambas imágenes
     NOMBRE_IMAGEN_OCULTA = "./img/question.png"; // La imagen que se muestra cuando la real está oculta
-new Vue({
+new Vue(
     el: "#app",
     data: () => ({
         // La ruta de las imágenes. Puede ser relativa o absoluta
